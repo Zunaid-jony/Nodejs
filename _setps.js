a15826e8f0b3686296bb7dd493d1d424e0d239db
unknown <jony35666666@gmail.com>
2022-09-29 22:41:22 +0600

    add

---
+++ _setps.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ * 1. create a folder 
+ * 2 open command line 
+ * 3  npm init -y 
+ * 4  npm install express
+ * 5  index.js
+ * 6  node app.js  {app.js mane index.js jeta file ami banisi oi ta}
+ * 
+ * 
+ * ...................
+ * for Express node server
+ * ..................
+ * 1
+ * 2. 
+ * 
+ */
--- index.js
+++ index.js
@@ -3,7 +3,34 @@ const app = express()
 const port = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World ! pagol')
+ 
+})
+
+
+const users = [
+    {
+        id:1 , name: 'jony', email:'md.jony.soft@gmail.com', phone:'01627392810'    
+    },
+    {
+        id:1 , name: 'rony', email:'md.jony.soft@gmail.com', phone:'01627392810'    
+    },
+    {
+        id:1 , name: 'mony', email:'md.jony.soft@gmail.com', phone:'01627392810'    
+    },
+    {
+        id:1 , name: 'abcd', email:'md.jony.soft@gmail.com', phone:'01627392810'    
+    }
+]
+app.get('/users',(req, res) =>{
+    res.send(users)
+})
+
+app.get('/user/:id', (req, res)=>{
+    console.log(req.params);
+    const id= req.params.id;
+    const user = users[id];
+    res.send(user)
 })
 
 app.listen(port, () => {
